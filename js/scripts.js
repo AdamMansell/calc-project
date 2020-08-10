22a7d26e4769ff1d35bf7f2829ed8a009b5cd180
AdamManselll <adammansellhmb@gmail.com>
2020-08-10 11:02:26 -0700

    added subtract, multiply, and division functions as well as alerts to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,34 @@
+// addition
 function add(number1, number2) {
   return number1 + number2;
+}
+// subtraction
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+//multiplication
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+//division
+function divide(number1, number2) {
+  return number1 / number2;
 }
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
-alert(result);
+// addition
+const resultAdd = add(number1, number2);
+alert(resultAdd);
+
+// subtraction
+const resultSub = subtract(number1, number2);
+alert(resultSub);
+
+//multiplication
+const resultMult = multiply(number1, number2);
+alert(resultMult);
+
+//division
+const resultDiv = divide(number1, number2);
+alert(resultDiv);
